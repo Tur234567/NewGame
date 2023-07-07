f47b9c1035f5072dcd28275c60be55a36e486c38
Tur234567 <facktick1910@mail.ru>
2023-07-07 16:49:29 +0500

    test work

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './JS/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "static", to: "static" }],
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+    }),
+        new HtmlWebpackPlugin({
+            template: "./game.html",
+    }),
+        new HtmlWebpackPlugin({
+            template: "./game-card.html",
+    }),
+],
+}
